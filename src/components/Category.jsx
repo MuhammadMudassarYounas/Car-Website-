6aa060672b33cca38b0d8d539cc39a5476d4bee2
Abdullah-536 <iamabdullah536@gmail.com>
2025-03-01 07:40:57 -0800

    Updated-1.0

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import Data from '../Shared/Data'
 function Category() {
+ 
   return (
+    
 <div className="mt-40">
     <h2 className="font-bold text-3xl text-center mb-6">Browse byTrype</h2>
-    <div>
+    <div className='grid grid-cols-3 sm:grid-cols-4 md:grid-cols-6 lg:grid-cols-9 gap-6 px-20'>
         {Data.Category.map((category,index)=>(
-            <div>
-                <img src={category.icon}  width={40} height={40}/>
+            <div className='border rounded-md p-3 items-center flex flex-col '>
+                <img src={category.img_src}  width={40} height={40}/>
+                <h2>{category.name}</h2>
             </div>
         ))}
     </div>
