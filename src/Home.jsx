b44562dd95579948398c92b00498073ea3d426ac
Abdullah-536 <iamabdullah536@gmail.com>
2025-03-02 10:51:14 -0800

    Carts_Added

--- src/Home.jsx
+++ src/Home.jsx
@@ -5,6 +5,8 @@ import Hero from "./components/Hero";
 import Category from "./components/Category";
 
 import Header from "./components/Header";
+import MostSearchedCar from "./components/MostSearchedCar";
+import InfoSection from "./components/InfoSection";
 
 
 
@@ -12,10 +14,11 @@ const Home=()=>{
  return(
     <div>
 
+      <Header/>
       <Hero/>
       <Category/>
-
-      <Header/>
+       <MostSearchedCar/>
+       <InfoSection/>
 
     </div>
  )
